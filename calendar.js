b1631189dfe47cc8f69f3c6f592fd1664d878e9f
Rufter <robert.kroeger@icloud.com>
2025-03-28 18:23:21 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,7 +1,7 @@
 document.addEventListener("animationstart", function(e) {
     if (e.animationName == 'navbarReady') {
 e.target.children[0].className = 'navbar-container'; 
-        document.getElementById('user-account-dropdown').insertAdjacentHTML('beforebegin','<a href="https://fadx-racing.de/race-calendar" target="_blank"><img src="https://fadx-racing.de/script/svg/calendar-22-svgrepo-com.svg" alt=""></img></a></div>');
+        document.getElementById('user-account-dropdown').insertAdjacentHTML('beforebegin','<a href="https://fadx-racing.de/race-calendar" target="_blank"><img src="https://cdn.jsdelivr.net/gh/Rufter/fadx-racing@main/calendar-22-svgrepo-com.svg" alt=""></img></a></div>');
         
     }
 }, false);
